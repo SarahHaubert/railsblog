6de43fb897065576d18bffd855742f3afbb9b6a8
Sarah Haubert <sarah.haubert@hotmail.com>
2022-04-30 08:48:29 -0600

    prepare for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Post.create(pip: "https://res.cloudinary.com/jklfdsjlfdjkslfsl/image/upload/v1651328295/trails/weehawken_v3timg.jpg", activity: "both", title: "Weehawken", body: "n the summer, this trail accesses big peaks like Potosi, Teakettle, and Whitehouse. In the winter, it's a popular location for storm skiing")
+
+Post.create(pip: "https://res.cloudinary.com/jklfdsjlfdjkslfsl/image/upload/v1651328294/trails/e_dag_oak_creek_ab0en1.jpg", activity: "run", title: "Oak Creek", body: "Oak Creek forms a big loop from the Perimeter Trail south of Twin Peaks to Silvershield a couple miles north of town.")
+
+Post.create(pip: "https://res.cloudinary.com/jklfdsjlfdjkslfsl/image/upload/v1651328304/trails/tim_and_pip_on_hosrethief_grhdpr.jpg", activity: "run", body: "Old Horsethief", detail: "The best way to access the Bridge of Heaven from town, and one of the top three big climbs in Ouray, Old Horsethief climbs 2,500' in about 2.5 miles.")
+
+Post.create(pip: "https://res.cloudinary.com/jklfdsjlfdjkslfsl/image/upload/v1651328295/trails/ice_park_rsaytn.jpg", activity: "run", title: "Ice Park Loop", body: "This 2 mile loop is a popular jaunt around both sides of the ice park, connected by bridges over the gorge")
+
+Post.create(pip: "https://res.cloudinary.com/jklfdsjlfdjkslfsl/image/upload/v1651328292/trails/camp_bird_o9p8ml.jpg", activity: "both", title: "Camp Bird Road", body: "This gently climbing dirt road is plowed all the way to the mine (about 7 miles from town) but only open to cars for the first 3 or so, so it's a popular place for winter and spring running")
+
+Post.create(pip: "https://res.cloudinary.com/jklfdsjlfdjkslfsl/image/upload/v1651328297/trails/perimeter_m6ds5n.jpg", activity: "run", title: "Perimter Trail", body: "Best 6 miles a person can find, this trail circles the town of Ouray and includes cliffs and exposure, several world class waterfalls, an old mining tunnel, about 1,200 feet of gain/loss and more!")
+
+Post.create(pip: "https://res.cloudinary.com/jklfdsjlfdjkslfsl/image/upload/v1651328295/trails/portland_iiwq3l.jpg", activity: "both", title: "Portland", body: "The Portland system encompasses the Portland road, Portland mine, and Portland trail, which forms a loop. Portland road is skiable, Portland trail generally gets packed most of the winter")
+
+Post.create(pip: "https://res.cloudinary.com/jklfdsjlfdjkslfsl/image/upload/v1651328295/trails/twin_peaks_fsbguf.jpg", activity: "run", title: "Twin Peaks", body: "Twin Peaks summit is accessed by Old Twin Peaks, steep and not for the faint of heart, or New Twin Peaks/Oak Creek. Best view in town!")
+
+Post.create(pip: "https://res.cloudinary.com/jklfdsjlfdjkslfsl/image/upload/v1651328295/trails/river_trail_uykhgq.jpg", activity: "both", title: "River Trail", body: "This two mile loop is connected by pedestrian bridges, gravel in the summer and groomed for nordic skiing in the winter")
+
+Post.create(pip: "https://res.cloudinary.com/jklfdsjlfdjkslfsl/image/upload/v1651328293/trails/ironton_w5samx.jpg", activity: "ski", title: "Ironton", body: "This is the main groomed nordic skiing area! Accessible from Red Mountain Pass, 9 miles south of town")
+
+Post.create(pip: "https://res.cloudinary.com/jklfdsjlfdjkslfsl/image/upload/v1651329028/trails/Sneff_umn9mc.jpg", activity: "both", title: "Mt. Sneffels", body: "Sneffels is Ouray's 14er, accessible from the Sneffels Trail at the end of Camp Bird road or from Ridgway via the Blue Lakes Trail. It's also a popular but dangerous ski mountaineering destination")
+
+Post.create(pip: "https://res.cloudinary.com/jklfdsjlfdjkslfsl/image/upload/v1651329111/trails/chief_ouray_uz8dyb.jpg", activity: "run", title: "Chief Ouray", body: "One of the three biggest climbs in town, Chief Ouray is accessed by the lower cascade falls system or the Chief Ouray cutoff from 5th street, climb 2,200ft in 2 miles to arrive at the Chief Ouray Mine")
+
+Post.create(pip: "https://res.cloudinary.com/jklfdsjlfdjkslfsl/image/upload/v1651329028/trails/n_hayden_etsfmb.jpg", activity: "run", title: "North Hayden", body: "Includes North Hayden Trail from Camp Bird Road and North Hayden Peak, accessible from the top of N Hayden Trail or from Half Moon Trail")
+
+Post.create(pip: "https://res.cloudinary.com/jklfdsjlfdjkslfsl/image/upload/v1651329482/trails/s_hayden_aprcue.jpg", activity: "run", title: "South Hayden", body: "Both the South Hayden Trail (from Crystal Lake) and South Hayden Peak (best accessed by Half Moon Trail but also accessible from the Hayden Ridge)")
+
+Post.create(pip: "https://res.cloudinary.com/jklfdsjlfdjkslfsl/image/upload/v1651329482/trails/mcmillan_gprjrl.jpg", activity: "ski", title: "McMillan", body: "Accessed from the top of Red Mountain Pass, this is the most mellow place to backcountry ski in the Ouray vicinity")
+
+Post.create(pip: "https://res.cloudinary.com/jklfdsjlfdjkslfsl/image/upload/v1651329487/trails/bear_creek_iz7dyu.jpg", activity: "run", title: "Bear Creek", body: "A National Recreation Trail, Bear Creek features some hard climbing, cliff exposure, several historic mines, and ultimately ends at Engineer Pass")
+
+Post.create(pip: "https://res.cloudinary.com/jklfdsjlfdjkslfsl/image/upload/v1651329483/trails/sutton_y4qsjr.jpg", activity: "run", title: "Sutton Mine", body: "This southside trail has a steep first mile then rolls parallel to the highway to the historic Sutton Mine")
